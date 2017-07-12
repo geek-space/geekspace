fe4e5c2ea9481dc2685e706b739c2d13b71e356a
Matt Valdez <mattvaldez01@gmail.com>
2017-07-11 23:35:29 -0500

    added browser tab logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>geekspace</title>
+        <link rel="shortcut icon" href="/img/gs.png" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">        
         <link rel="stylesheet" type="text/css" href="css/styles.css">
