bd5e5099e57b24d0be5353cc9b1d6fccc2f0a502
Matt Valdez <mattvaldez01@gmail.com>
2017-07-13 02:06:06 -0500

    renamed styles.css to landing-styles.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link rel="shortcut icon" href="/img/gs.png" type="image/x-icon">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">        
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="stylesheet" type="text/css" href="css/landing-styles.css">
     </head>
 
     <body>
