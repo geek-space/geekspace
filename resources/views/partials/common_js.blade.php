461d10b99cca3918a904b33ebde4d77b45871d07
Matt Valdez <mattvaldez01@gmail.com>
2017-07-13 02:09:50 -0500

    created partials for head, navbar, and js

---
+++ resources/views/partials/common_js.blade.php
@@ -0,0 +1,11 @@
+<!-- JQUERY -->
+<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+<!-- BOOTSTRAP -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- CUSTOM JS-->
+<script>
+(function() {
+    "use strict";
+    
+})();
+</script>
