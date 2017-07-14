bd97481007fdeda85de3d4e44ddacb9df0bb0096
Matt Valdez <mattvaldez01@gmail.com>
2017-07-14 09:17:30 -0500

    created footer partial

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer navbar-inverse">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 footer footer-links">
+                <a href="/home">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <div class="col-md-12 footer socialMedia">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+            <div class="col-md-12 footer copyright">
+                <p>&copy; 2017 geekspace</p>
+            </div>
+        </div>
+    </div>
+</footer>
